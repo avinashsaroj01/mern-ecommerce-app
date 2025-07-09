296998ed4fa940998fa293698add69756bb1c74c
Avinash Saroj <98206280+avinashsaroj01@users.noreply.github.com>
2025-07-09 16:43:36 +0530

    Basic homePage setup an routing added.

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Home from "./pages/Home";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
